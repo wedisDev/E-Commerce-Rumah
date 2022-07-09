12131771626022f3ae3cb5f7dd14cf60dd29d45a
bayunugroho2022 <bayu.nugroho@kpvendor.id>
2022-07-09 19:51:57 +0700

    feat: fix dashboard seller

--- app/src/main/java/com/example/rumah/penjual/DashboardPenjual.java
+++ app/src/main/java/com/example/rumah/penjual/DashboardPenjual.java
@@ -6,23 +6,20 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.rumah.R;
 
 public class DashboardPenjual extends AppCompatActivity implements View.OnClickListener {
-    Button signOut;
     CardView a,b,c,d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_penjual);
 
-        signOut = ( Button) findViewById(R.id.signOut);
-        a = findViewById(R.id.a);
-        b = findViewById(R.id.b);
-        c = findViewById(R.id.c);
-        d = findViewById(R.id.d);
+        a = (CardView) findViewById(R.id.a);
+        b = (CardView) findViewById(R.id.b);
+        c = (CardView) findViewById(R.id.c);
+        d = (CardView) findViewById(R.id.d);
     }
 
     @Override
@@ -31,22 +28,18 @@ public class DashboardPenjual extends AppCompatActivity implements View.OnClickL
             case R.id.a:
                 Intent list = new Intent(DashboardPenjual.this, daftarRumah.class);
                 DashboardPenjual.this.startActivity(list);
-                finish();
                 break;
             case R.id.b:
                 Intent jual = new Intent(DashboardPenjual.this, iklanRumah.class);
                 DashboardPenjual.this.startActivity(jual);
-                finish();
                 break;
             case R.id.c:
                 Intent update = new Intent(DashboardPenjual.this, updateRumah.class);
                 DashboardPenjual.this.startActivity(update);
-                finish();
                 break;
             case R.id.d:
                 Intent akun = new Intent(DashboardPenjual.this, akunPenjual.class);
                 DashboardPenjual.this.startActivity(akun);
-                finish();
                 break;
         }
     }
