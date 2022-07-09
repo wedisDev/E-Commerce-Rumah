efe21a0a9dffac6856690ba4d9e49dd2d20289f1
bayunugroho2022 <bayu.nugroho@kpvendor.id>
2022-07-09 19:54:37 +0700

    chore: add response login

---
+++ app/src/main/java/com/example/rumah/data/network/response/login/ResponseLogin.java
@@ -0,0 +1,14 @@
+package com.example.rumah.data.network.response.login;
+
+public class ResponseLogin{
+	private Data data;
+	private String status;
+
+	public Data getData(){
+		return data;
+	}
+
+	public String getStatus(){
+		return status;
+	}
+}
