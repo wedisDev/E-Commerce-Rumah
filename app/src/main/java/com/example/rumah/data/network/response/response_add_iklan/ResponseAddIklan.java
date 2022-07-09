eb7c57195fb1dca566bc564dba6eaad11405737d
bayunugroho2022 <bayu.nugroho@kpvendor.id>
2022-07-09 19:55:10 +0700

    chore: add response (add iklan)

---
+++ app/src/main/java/com/example/rumah/data/network/response/response_add_iklan/ResponseAddIklan.java
@@ -0,0 +1,28 @@
+package com.example.rumah.data.network.response.response_add_iklan;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseAddIklan{
+
+	@SerializedName("success")
+	private boolean success;
+
+	@SerializedName("message")
+	private String message;
+
+	public void setSuccess(boolean success){
+		this.success = success;
+	}
+
+	public boolean isSuccess(){
+		return success;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+}
