c9a59cf0a1e99f7f9c261772b2e1cbfdd4f1bc4f
bayunugroho2022 <bayu.nugroho@kpvendor.id>
2022-07-09 19:51:06 +0700

    chore: add constan file

---
+++ app/src/main/java/com/example/rumah/data/Constant.java
@@ -0,0 +1,6 @@
+package com.example.rumah.data;
+
+public class Constant {
+    public static final String baseURL = "http://192.168.100.113:8000/";
+    public static final String baseImageURL = "http://192.168.100.113:8000/images/";
+}
