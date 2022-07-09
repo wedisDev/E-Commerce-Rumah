dcc6b4d359b9807d2c1320596d0ad228fad939fd
bayunugroho2022 <bayu.nugroho@kpvendor.id>
2022-07-09 19:49:20 +0700

    chore: fix adapter

--- app/src/main/java/com/example/rumah/adapter/adapterRumah.java
+++ app/src/main/java/com/example/rumah/adapter/adapterRumah.java
@@ -1,9 +1,12 @@
 package com.example.rumah.adapter;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,55 +14,83 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.rumah.R;
-import com.example.rumah.model.ModelRumah;
-import androidx.activity.result.contract.ActivityResultContracts;
+import com.example.rumah.data.Constant;
+import com.example.rumah.data.network.response.get_rumah.DataItem;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class adapterRumah extends RecyclerView.Adapter<adapterRumah.ViewHolder> {
 
-    private ArrayList<ModelRumah> rumahlist;
+    private ArrayList<DataItem> rumahlist;
 //    String judul,alamat,kamar,mandi;
 
-    public adapterRumah (ArrayList<ModelRumah> rumahlist){
-        this.rumahlist=rumahlist;
+    public adapterRumah(ArrayList<DataItem> rumahlist) {
+        this.rumahlist = rumahlist;
     }
-
 
 
     @NonNull
     @Override
     public adapterRumah.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_rumah,parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_rumah, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        ModelRumah mr=rumahlist.get(position);
-//        Glide.with(holder.itemView.getContext()).load(mr.getkamar()).into(holder.tkamar);
-        holder.tjudul.setText(mr.getJudul());
-        holder.talamat.setText(mr.getAlamat());
-        holder.tkamar.setText(mr.getkamar());
-        holder.tmandi.setText(mr.getmandi());
+        DataItem mr = rumahlist.get(position);
+        Glide.with(holder.itemView.getContext()).load(Constant.baseImageURL + mr.getGambar()).into(holder.iv_gambar);
+        holder.tjudul.setText(mr.getJudulRumah());
+        holder.talamat.setText(mr.getHargaRumah());
+
+        holder.btn_detail_daftar_rumah.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(view.getContext());
+                View dialogView= LayoutInflater.from(view.getContext()).inflate(R.layout.dialog_detail_rumah,null);
+                TextView tv_detail_judul, tv_detail_alamat, tv_detail_harga, tv_detail_desc;
+                ImageView iv_detail_gambar;
+
+                tv_detail_judul = dialogView.findViewById(R.id.tv_detail_judul);
+                tv_detail_alamat = dialogView.findViewById(R.id.tv_detail_alamat);
+                tv_detail_harga = dialogView.findViewById(R.id.tv_detail_harga);
+                tv_detail_desc = dialogView.findViewById(R.id.tv_detail_desc);
+                iv_detail_gambar = dialogView.findViewById(R.id.iv_detail_gambar);
+
+                Glide.with(holder.itemView.getContext())
+                        .load(Constant.baseImageURL + mr.getGambar())
+                        .into(iv_detail_gambar);
+
+                tv_detail_judul.setText("Judul Rumah: " + mr.getJudulRumah());
+                tv_detail_alamat.setText("Alamat Rumah: " + mr.getKelurahan()+", "+mr.getAlamatRumah());
+                tv_detail_harga.setText("Harga Rumah: " + mr.getHargaRumah());
+                tv_detail_desc.setText("Deskripsi Rumah: " + mr.getDescRumah());
+
+                builder.setView(dialogView);
+                builder.setCancelable(true);
+                builder.show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return rumahlist.size() ;
+        return rumahlist.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView tjudul,talamat,tkamar,tmandi;
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tjudul, talamat;
+        ImageView iv_gambar;
+        Button btn_detail_daftar_rumah;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
-            tjudul = itemView.findViewById(R.id.judul);
-            talamat = itemView.findViewById(R.id.alamat);
-            tkamar = itemView.findViewById(R.id.kamar);
-            tmandi = itemView.findViewById(R.id.bathup);
+            tjudul = itemView.findViewById(R.id.tv_judul_daftar_rumah);
+            iv_gambar = itemView.findViewById(R.id.iv_gambar);
+            btn_detail_daftar_rumah = itemView.findViewById(R.id.btn_detail_daftar_rumah);
+            talamat = itemView.findViewById(R.id.tv_alamat_daftar_rumah);
         }
 
     }
