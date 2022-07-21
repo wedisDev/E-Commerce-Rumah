62a038d33eda785caaa662ef230374076ca3ee6c
Denandra Prasetya <denandra1628@gmail.com>
2022-07-21 16:13:22 +0700

    feature: add detail fragmen

--- app/src/main/java/com/example/rumah/pembeli/DetailLahanActivity.java
+++ app/src/main/java/com/example/rumah/pembeli/DetailLahanActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +27,7 @@ public class DetailLahanActivity extends AppCompatActivity {
 
     TextView tv_detail_judul, tv_detail_alamat, tv_detail_harga, tv_detail_desc, tv_detail_pemilik, tv_detail_tgl;
     Button btn_detail_beli;
+    ImageButton back_button;
     ImageView iv_detail_gambar;
 
     @SuppressLint("SetTextI18n")
@@ -39,23 +41,22 @@ public class DetailLahanActivity extends AppCompatActivity {
 
         tv_detail_judul = findViewById(R.id.tv_detail_judul);
         tv_detail_tgl = findViewById(R.id.tv_detail_tgl);
-        tv_detail_alamat = findViewById(R.id.tv_detail_alamat);
         tv_detail_harga = findViewById(R.id.tv_detail_harga);
         tv_detail_desc = findViewById(R.id.tv_detail_desc);
         iv_detail_gambar = findViewById(R.id.iv_detail_gambar);
         tv_detail_pemilik = findViewById(R.id.tv_detail_pemilik);
         btn_detail_beli = findViewById(R.id.btn_detail_beli);
+        back_button = findViewById(R.id.backButton);
 
         Glide.with(getApplicationContext())
                 .load(Constant.baseImageURL + mr.getGambar())
                 .centerCrop()
                 .into(iv_detail_gambar);
-        tv_detail_pemilik.setText("Pemilik : " + mr.getPenjual());
-        tv_detail_judul.setText("Judul Rumah : " + mr.getJudulRumah());
-        tv_detail_alamat.setText("Alamat Rumah : " + mr.getKelurahan()+", "+mr.getAlamatRumah());
-        tv_detail_harga.setText("Harga Rumah : " + mr.getHargaRumah());
-        tv_detail_desc.setText("Deskripsi Rumah : " + mr.getDescRumah());
-        tv_detail_tgl.setText("Tanggal Penjualan : " + mr.getTgl());
+        tv_detail_pemilik.setText( mr.getPenjual());
+        tv_detail_judul.setText(mr.getJudulRumah());
+        tv_detail_harga.setText(mr.getHargaRumah());
+        tv_detail_desc.setText(mr.getDescRumah());
+        tv_detail_tgl.setText(mr.getTgl());
 
         if(isPenjual){
             btn_detail_beli.setVisibility(View.GONE);
@@ -65,27 +66,32 @@ public class DetailLahanActivity extends AppCompatActivity {
         String idPengguna = SharedPref.getIdPengguna(getApplicationContext());
 
         btn_detail_beli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EndPoint endPoint = ApiClient.getClient().create(EndPoint.class);
+                Call<ResponseSuccess> call = endPoint.beliRumah(idPengguna, String.valueOf(mr.getId()),mr.getEmailPenjual());
+                call.enqueue(new retrofit2.Callback<ResponseSuccess>() {
                     @Override
-                    public void onClick(View view) {
-                        EndPoint endPoint = ApiClient.getClient().create(EndPoint.class);
-                        Call<ResponseSuccess> call = endPoint.beliRumah(idPengguna, String.valueOf(mr.getId()),
-                                mr.getEmailPenjual());
-                        call.enqueue(new retrofit2.Callback<ResponseSuccess>() {
-                            @Override
-                            public void onResponse(Call<ResponseSuccess> call, retrofit2.Response<ResponseSuccess> response) {
-                                if(response.body().getMessage().equals("OK")){
-                                    Toast.makeText(view.getContext(), "Sukses", Toast.LENGTH_SHORT).show();
-                                }else{
-                                    Toast.makeText(view.getContext(), "data sudah tersedia", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(Call<ResponseSuccess> call, Throwable t) {
-                                Log.d("response", t.getMessage());
-                            }
-                        });
+                    public void onResponse(Call<ResponseSuccess> call, retrofit2.Response<ResponseSuccess> response) {
+                        if(response.body().getMessage().equals("OK")){
+                            Toast.makeText(view.getContext(), "Sukses", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(view.getContext(), "data sudah tersedia", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<ResponseSuccess> call, Throwable t) {
+                        Log.d("response", t.getMessage());
                     }
                 });
+            }
+        });
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
